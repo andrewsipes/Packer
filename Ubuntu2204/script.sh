cf5dc1d30dc64ed209e2110c49d6bf24c417bdeb
unknown <andrew.sipes@outlook.com>
2025-08-07 09:40:02 -0700

    Working generalization workflow

--- Ubuntu2204/script.sh
+++ Ubuntu2204/script.sh
@@ -6,21 +6,47 @@ sudo apt update
 #insert generalization script
 sudo tee ~/generalize.sh > /dev/null << 'EOF'
 #!/bin/bash
+
+COUNTER="/home/ubuntu/counter.txt"
+REBOOTS=1
+
+# Create counter file if not exists
+if [ ! -e "$COUNTER" ]; then
+    echo 0 > /home/ubuntu/counter.txt
+fi
+
+# Set Count variable
+count=$(cat "$COUNTER")
+
+# Generalize
 sudo rm -f /etc/machine-id
 sudo systemd-machine-id-setup
 sudo ln -sf /etc/machine-id /var/lib/dbus/machine-id
 sudo rm /etc/ssh/ssh_host_*
 sudo ssh-keygen -A
 sudo systemctl restart sshd
-echo "Generalized on $(date)" >> /var/log/generalize_log.txt
+sudo echo "Generalized on $(date)" | sudo tee -a /var/log/generalize_log.txt > /dev/null
+
+# If count is less than target, increment and exit
+if [ "$count" -lt "$REBOOTS" ]; then
+    count=$((count + 1))
+    echo "$count" > "$COUNTER"
+    exit 0
+fi
+
+# remove cron job
+sudo sed -i '\@reboot root /home/ubuntu/generalize.sh|d' /etc/crontab
+
+sudo rm -f /home/ubuntu/generalize.sh
+sudo rm -f /home/ubuntu/counter.txt
+
 EOF
 
-#change execution parameters
+# change execution parameters
 sudo chmod +x ~/generalize.sh
 
-#create cron job
-sudo su
-echo "@reboot root ~/generalize.sh" | sudo tee -a /etc/crontab
-
-#require password on first boot
+# require password on first boot
 sudo passwd -e ubuntu
+
+# create cron job
+sudo bash -c 'echo "@reboot root /home/ubuntu/generalize.sh" >> /etc/crontab'
